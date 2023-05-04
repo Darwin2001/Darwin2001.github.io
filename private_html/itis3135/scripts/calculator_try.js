9ca02e030b6ac23ad3bc3418844aeca166b18a0e
Darwin Peraza <darwinperaza2001@gmail.com>
2023-05-04 02:01:45 -0400

    Completed arrays.html
    Did the array.html assignment

---
+++ private_html/itis3135/scripts/calculator_try.js
@@ -0,0 +1,9 @@
+function getValue(value){
+    document.getElementById("total").value += value
+}
+
+function clear(){
+    document.getElementById("total").value = "0"
+}   
+
+//Didn't finish cause didn't really know how uk 
